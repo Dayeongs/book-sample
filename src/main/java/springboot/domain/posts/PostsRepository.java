4cbfcd8a9432b0118c013b8ee393ecf1f37a56c4
Dayeongs <ekdud9636@gmail.com>
2024-06-17 14:35:27 +0900

    게시글 수정, 삭제 기능 구현 및 소셜 로그인 기능 구현 ~ 223P

--- src/main/java/springboot/domain/posts/PostsRepository.java
+++ src/main/java/springboot/domain/posts/PostsRepository.java
@@ -1,6 +1,14 @@
 package springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    // SpringDataJpa에서 제공하지 않는 메소드는 @Query 어노테이션을 사용하여 쿼리로 작성할 수 있다.
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
+
 }
--- src/main/java/springboot/service/posts/PostsService.java
+++ src/main/java/springboot/service/posts/PostsService.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import springboot.domain.posts.Posts;
 import springboot.domain.posts.PostsRepository;
+import springboot.web.dto.PostsListResponseDto;
 import springboot.web.dto.PostsResponseDto;
 import springboot.web.dto.PostsSaveRequestDto;
 import springboot.web.dto.PostsUpdateRequestDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,6 +38,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되, 조회 기능만 남겨두어 조회 속도가 개선됨
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
     }
 
 }
